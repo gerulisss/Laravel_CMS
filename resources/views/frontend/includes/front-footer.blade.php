925844ca3f5b9d8882c52ff0d65aab62b06142c1
RokasM <34399728+gerulisss@users.noreply.github.com>
2022-08-07 20:17:21 +0300

    Update front-footer.blade.php

--- resources/views/frontend/includes/front-footer.blade.php
+++ resources/views/frontend/includes/front-footer.blade.php
@@ -1,12 +1,10 @@
 <footer class="bg-light text-center text-lg-start">
-    <!-- Copyright -->
     <div class="text-left p-3" style="background-color: rgba(0, 0, 0, 0.2);">
       &copy Copyright {{ date('Y') }}
-      <a class="text-dark" href="https://zmfood.eu/">"ZMFOOD" UAB</a>
+      <a class="text-dark" href="https://yourwebsite.com">Your website name</a>
 
       All Rights Reserved.
         <a href="https://{{ env('DEVELOPER_WEBSITE') }}.lt" class="text-dark">{{ env('DEVELOPER_COPYRIGHTS') }}</a>
         {{ __('Conctact information') }} <a href="mailto:{{ env('CONTACT_EMAIL') }}">{{ env('CONTACT_EMAIL') }}</a>
     </div>
-        <!-- Copyright -->
   </footer>
